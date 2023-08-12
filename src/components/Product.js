e9b02e759e72dfeae57ec0b183d02fee56a572d7
bemaldvanitha <bdvidanagamachchi@gmail.com>
2023-08-12 18:59:28 +0530

    custom styles added and rating component added

--- src/components/Product.js
+++ src/components/Product.js
@@ -2,6 +2,8 @@ import React from "react";
 import { Card } from "react-bootstrap";
 import { Link } from 'react-router-dom';
 
+import Rating from './Rating';
+
 const Product = ({ product }) => {
     return(
         <Card className={'my-3 p-3 rounded'}>
@@ -10,7 +12,7 @@ const Product = ({ product }) => {
             </Link>
             <Card.Body>
                 <Link to={`/product/${product._id}`}>
-                    <Card.Title as={'div'}>
+                    <Card.Title as={'div'} className={'product-title'}>
                         <strong>{ product.name }</strong>
                     </Card.Title>
                 </Link>
@@ -18,6 +20,11 @@ const Product = ({ product }) => {
                 <Card.Text as={'h3'}>
                     ${ product.price }
                 </Card.Text>
+
+                <Card.Text as={'div'}>
+                    <Rating value={ product.rating } text={`${product.numReviews} reviews`}/>
+                </Card.Text>
+
             </Card.Body>
         </Card>
     )
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import 'bootstrap/dist/css/bootstrap.min.css';
+//import 'bootstrap/dist/css/bootstrap.min.css';
+import './assets/styles/bootstrap.custom.css';
+import './assets/styles/index.css';
 import './index.css';
 import { createBrowserRouter, createRoutesFromElements,
     Route, RouterProvider } from 'react-router-dom';
