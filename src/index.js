57a66c7e0a9cecf62fb91a16f05734d5cf18b1c8
bemaldvanitha <bdvidanagamachchi@gmail.com>
2023-08-12 18:43:08 +0530

    router added

--- src/index.js
+++ src/index.js
@@ -2,12 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
+import { createBrowserRouter, createRoutesFromElements,
+    Route, RouterProvider } from 'react-router-dom';
+
+import HomeScreen from "./screens/HomeScreen";
 import App from './App';
+
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path={'/'} element={<App/>}>
+            <Route path={'/'} element={<HomeScreen/>} index={true}/>
+        </Route>
+    )
+)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
 
