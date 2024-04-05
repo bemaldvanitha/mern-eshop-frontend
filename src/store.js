8f48f6c615e2bca59ecef53bb68824fb6d22ed50
bemal-engenuityai <141420637+bemal-engenuityai@users.noreply.github.com>
2024-04-06 05:13:50 +0530

    cart slice reducer added

--- src/store.js
+++ src/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import { apiSlice } from "./slicers/apiSlice";
+import cartSliceReducer from './slicers/cartSlice';
 
 const store = configureStore({
    reducer: {
-      [apiSlice.reducerPath] : apiSlice.reducer
+      [apiSlice.reducerPath] : apiSlice.reducer,
+      cart: cartSliceReducer
    },
    middleware: (getDefaultMiddleware) => getDefaultMiddleware()
        .concat(apiSlice.middleware),
