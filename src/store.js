8ce7c08651610e03c1ffadd2a2719b88d26e1e73
bemal-engenuityai <141420637+bemal-engenuityai@users.noreply.github.com>
2024-04-11 04:48:16 +0530

    user and auth slice added

--- src/store.js
+++ src/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import { apiSlice } from "./slicers/apiSlice";
 import cartSliceReducer from './slicers/cartSlice';
+import authSliceReducer from "./slicers/authSlice";
 
 const store = configureStore({
    reducer: {
       [apiSlice.reducerPath] : apiSlice.reducer,
-      cart: cartSliceReducer
+      cart: cartSliceReducer,
+      auth: authSliceReducer,
    },
    middleware: (getDefaultMiddleware) => getDefaultMiddleware()
        .concat(apiSlice.middleware),
