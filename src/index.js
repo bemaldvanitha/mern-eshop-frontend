ea090d35b029a966c1b616e0c99507886a9d6986
bemal-engenuityai <141420637+bemal-engenuityai@users.noreply.github.com>
2024-04-12 05:31:03 +0530

    private route added

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ import CartScreen from "./screens/CartScreen";
 import LoginScreen from "./screens/LoginScreen";
 import RegisterScreen from "./screens/RegisterScreen";
 import ShippingScreen from "./screens/ShippingScreen";
+import PrivateRoute from "./components/PrivateRoute";
 import App from './App';
 import store from "./store";
 
@@ -26,7 +27,9 @@ const router = createBrowserRouter(
             <Route path={'/cart'} element={<CartScreen/>}/>
             <Route path={'/login'} element={<LoginScreen/>}/>
             <Route path={'/register'} element={<RegisterScreen/>}/>
-            <Route path={'/shipping'} element={<ShippingScreen/>}/>
+            <Route path={''} element={<PrivateRoute/>}>
+                <Route path={'/shipping'} element={<ShippingScreen/>}/>
+            </Route>
         </Route>
     )
 )
