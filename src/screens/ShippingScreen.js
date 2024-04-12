5fde1f6e15f0bc91143c037ed44e680a80743050
bemal-engenuityai <141420637+bemal-engenuityai@users.noreply.github.com>
2024-04-12 05:41:07 +0530

    checkout step component add

--- src/screens/ShippingScreen.js
+++ src/screens/ShippingScreen.js
@@ -5,6 +5,7 @@ import { useNavigate } from "react-router-dom";
 
 import { saveShippingAddress } from "../slicers/cartSlice";
 import FormContainer from "../components/FormContainer";
+import CheckoutSteps from "../components/CheckoutSteps";
 
 const ShippingScreen = () => {
     const dispatch = useDispatch();
@@ -26,6 +27,7 @@ const ShippingScreen = () => {
 
     return(
         <FormContainer>
+            <CheckoutSteps step1 step2/>
             <h1>Shipping</h1>
             <Form onSubmit={submitHandler}>
                 <Form.Group controlId={'address'} className={'my-2'}>
